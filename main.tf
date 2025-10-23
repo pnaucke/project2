5a91f62a9a1016e033d96a627daa7252e575dbab
Pieter Naucke <554603@student.fontys.nl>
2025-10-23 15:51:57 +0200

    v0.21

--- main.tf
+++ main.tf
@@ -91,39 +91,11 @@ resource "random_id" "suffix" {
 }
 
 # ----------------------
-# Security Groups
+# Security Groups (create SGs first without cross-references)
 # ----------------------
 resource "aws_security_group" "web_sg" {
   name   = "web-sg-${random_id.suffix.hex}"
   vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
-    security_groups = [aws_security_group.grafana_sg.id]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["82.170.150.87/32", "145.93.76.108/32"]
-  }
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
-  }
-
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
-  }
 
   egress {
     from_port   = 0
@@ -137,20 +109,6 @@ resource "aws_security_group" "db_sg" {
   name   = "db-sg-${random_id.suffix.hex}"
   vpc_id = data.aws_vpc.default.id
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
-
-  ingress {
-    from_port       = 9100
-    to_port         = 9100
-    protocol        = "tcp"
-    security_groups = [aws_security_group.grafana_sg.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -163,27 +121,6 @@ resource "aws_security_group" "soar_sg" {
   name   = "soar-sg-${random_id.suffix.hex}"
   vpc_id = data.aws_vpc.default.id
 
-  ingress {
-    from_port       = 9100
-    to_port         = 9100
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.db_sg.id]
-  }
-
-  ingress {
-    from_port       = 9090
-    to_port         = 9090
-    protocol        = "tcp"
-    security_groups = [aws_security_group.grafana_sg.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -195,41 +132,6 @@ resource "aws_security_group" "soar_sg" {
 resource "aws_security_group" "grafana_sg" {
   name   = "grafana-sg-${random_id.suffix.hex}"
   vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port       = 9100
-    to_port         = 9100
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
-
-  ingress {
-    from_port       = 9090
-    to_port         = 9090
-    protocol        = "tcp"
-    security_groups = [aws_security_group.soar_sg.id]
-  }
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.db_sg.id]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["82.170.150.87/32", "145.93.76.108/32"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["82.170.150.87/32", "145.93.76.108/32"]
-  }
 
   egress {
     from_port   = 0
@@ -263,6 +165,141 @@ resource "aws_security_group" "lb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# ----------------------
+# Security Group Rules (cross-SG references as separate resources)
+# ----------------------
+
+# Webserver Rules
+resource "aws_security_group_rule" "web_from_lb_http" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.web_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
+}
+
+resource "aws_security_group_rule" "web_from_lb_https" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.web_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
+}
+
+resource "aws_security_group_rule" "web_from_grafana" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.web_sg.id
+  source_security_group_id = aws_security_group.grafana_sg.id
+}
+
+resource "aws_security_group_rule" "web_ssh_from_admins" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web_sg.id
+  cidr_blocks       = ["82.170.150.87/32", "145.93.76.108/32"]
+}
+
+# DB Rules
+resource "aws_security_group_rule" "db_from_web" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_sg.id
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "db_from_grafana" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_sg.id
+  source_security_group_id = aws_security_group.grafana_sg.id
+}
+
+# SOAR Rules
+resource "aws_security_group_rule" "soar_from_web" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.soar_sg.id
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "soar_from_db" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.soar_sg.id
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
+resource "aws_security_group_rule" "soar_from_grafana" {
+  type                     = "ingress"
+  from_port                = 9090
+  to_port                  = 9090
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.soar_sg.id
+  source_security_group_id = aws_security_group.grafana_sg.id
+}
+
+# Grafana Rules
+resource "aws_security_group_rule" "grafana_from_web" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.grafana_sg.id
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "grafana_from_soar" {
+  type                     = "ingress"
+  from_port                = 9090
+  to_port                  = 9090
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.grafana_sg.id
+  source_security_group_id = aws_security_group.soar_sg.id
+}
+
+resource "aws_security_group_rule" "grafana_from_db" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.grafana_sg.id
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
+# Grafana public access
+resource "aws_security_group_rule" "grafana_http_public" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  security_group_id = aws_security_group.grafana_sg.id
+  cidr_blocks       = ["82.170.150.87/32", "145.93.76.108/32"]
+}
+
+resource "aws_security_group_rule" "grafana_ssh_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.grafana_sg.id
+  cidr_blocks       = ["82.170.150.87/32", "145.93.76.108/32"]
 }
 
 # ----------------------
