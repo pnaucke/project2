581545a0ec8db2ef55ac193aa1374f62df0db243
Pieter Naucke <554603@student.fontys.nl>
2025-10-23 10:42:59 +0200

    v0.13

--- main.tf
+++ main.tf
@@ -24,27 +24,27 @@ data "aws_vpc" "default" {
 }
 
 # ----------------------
-# Subnets (namen en IP's aangepast)
+# Subnets
 # ----------------------
 resource "aws_subnet" "web1_subnet" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "172.31.10.0/24"
+  cidr_block              = "172.31.1.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
-  tags = { Name = "web-subnet-1" }
+  tags = { Name = "web1-subnet" }
 }
 
 resource "aws_subnet" "web2_subnet" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "172.31.11.0/24"
+  cidr_block              = "172.31.2.0/24"
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
-  tags = { Name = "web-subnet-2" }
+  tags = { Name = "web2-subnet" }
 }
 
 resource "aws_subnet" "db_subnet1" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "172.31.20.0/24"
+  cidr_block              = "172.31.3.0/24"
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = false
   tags = { Name = "db-subnet-1" }
@@ -52,7 +52,7 @@ resource "aws_subnet" "db_subnet1" {
 
 resource "aws_subnet" "db_subnet2" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "172.31.21.0/24"
+  cidr_block              = "172.31.4.0/24"
   availability_zone       = "eu-central-1c"
   map_public_ip_on_launch = false
   tags = { Name = "db-subnet-2" }
